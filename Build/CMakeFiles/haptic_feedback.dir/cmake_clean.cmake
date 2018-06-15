ec39a496232c3bc193ac54b13725ce841272167b
thatchayut <thatchayut@Totay-NUC.localdomain>
2018-06-15 13:27:09 +0900

    write instruction to readme.txt

---
+++ Build/CMakeFiles/haptic_feedback.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haptic_feedback.dir/haptic_feedback.cpp.o"
+  "haptic_feedback.pdb"
+  "haptic_feedback"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/haptic_feedback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# haptic_feedback
+ # haptic_feedback
+ # use this command to compile
+	 g++ -O3 -Wall -I/usr/local/include -Winline -pipe -L/usr/local/lib haptic_feedback.cpp  -lwiringPi -lwiringPiDev -lpthread -lm -lcrypt -lrt -o haptic_feedback
+ # use this command to run
+	./haptic_feedback
