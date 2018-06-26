fd73c91ae238cde4feb8b2cb3f2d5db35799e52a
thatchayut <thatchayut.u@gmail.com>
2018-06-26 11:22:27 +0900

    delete unnecessary codes

--- haptic_feedback.cpp
+++ haptic_feedback.cpp
@@ -5,7 +5,6 @@
 #include <wiringPi.h>
 
 #define pin_out_GPIO_7 7 //physical pin = 7
-#define pin_out_GPIO_21 21 //physical pin = 29
 
 using namespace std;
 
@@ -16,20 +15,19 @@ int main(void)
     wiringPiSetup();
     cout << "\nwiring Pi is completely set up...";
     delay(200);
+
     //define pin mode
     pinMode(pin_out_GPIO_7, OUTPUT); 
-    pinMode(pin_out_GPIO_21, OUTPUT);
     cout << "\npins are completely setup...";
     delay(200);
+
     //turn on the light 
     while(true)
     {
         cout << "\nLoop is accessible";
-        //digitalWrite(pin_out_GPIO_7, LOW);
-        digitalWrite(pin_out_GPIO_21, LOW);
+        digitalWrite(pin_out_GPIO_7, LOW); //L298N module is active LOW
         delay(1000);
-	//digitalWrite(pin_out_GPIO_7, HIGH);
-	digitalWrite(pin_out_GPIO_21, HIGH);
+	digitalWrite(pin_out_GPIO_7, HIGH);
 	delay(1000);
     }
     
