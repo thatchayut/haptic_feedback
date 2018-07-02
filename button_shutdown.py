d868fe384f10ffe96acae5c6f76db5404cae9154
thatchayut <thatchayut.u@gmail.com>
2018-07-02 11:44:06 +0900

    optimize the shutdown function

--- button_shutdown.py
+++ button_shutdown.py
@@ -2,21 +2,27 @@ from gpiozero import Button
 from subprocess import check_call
 from signal import pause
 import time
+import RPi.GPIO as GPIO
+
+pin_out_BCM23 = 23 # BCM23 physical pin = 16 connected with RED LED
 
 def shutdown():
     print('The system is shutting down...')
-    time.sleep(2)
+    count = 0
+    while (count < 3):
+    	GPIO.output(pin_out_BCM23, GPIO.LOW)
+	time.sleep(0.3)
+	GPIO.output(pin_out_BCM23, GPIO.HIGH)
+        time.sleep(0.3)
+	count += 1
     check_call(['sudo', 'poweroff'])
 
-def showInformation():
-    print('This device uses the safety shutdown...')
-    print('Press the button 3 seconds to shut down...')
-    print('')
-
 def main():
-    shutdown_btn = Button(17, hold_time=3) # pin number is in BCM && physical pin = 11
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin_out_BCM23, GPIO.OUT)
 
-    shutdown_btn.when_pressed = showInformation
+    shutdown_btn = Button(17, hold_time=3) # pin number is in BCM && physical pin = 11
 
     shutdown_btn.when_held = shutdown 
 
